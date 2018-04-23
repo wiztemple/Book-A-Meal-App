9b3bbcda22e113de42619ea2e28ac0e611597217
wiztemple <wiztemple7@gmail.com>
2018-04-23 15:51:44 +0100

    add mealController to the controllers folder, create an addMeal method for adding meal options

---
+++ dummyServer/controllers/mealController.js
@@ -0,0 +1,46 @@
+import database from '../dummyData';
+
+const { meals } = database;
+
+class mealController {
+  static addMeal(req, res) {
+    const {
+      mealTitle,
+      description,
+      price,
+      imageUrl,
+    } = req.body;
+    const id = meals[meals.length - 1].id + 1;
+    const addedMeal = {
+      id,
+      mealTitle,
+      description,
+      price,
+      imageUrl,
+    };
+    const foundMeal = meals.find(meal =>
+      (meal.mealTitle.toLowerCase() === mealTitle.toLowerCase()));
+    if (!foundMeal) {
+      meals.push(addedMeal);
+      return res.status(201).json({
+        meal: addedMeal,
+        status: 'Success',
+        message: 'Meal added successfully',
+      });
+    }
+    if (foundMeal) {
+      if (foundMeal.id === id) {
+        return res.status(409).json({
+          message: `A meal with this '${id}' is already in the meal options`,
+          status: 'Fail',
+        });
+      }
+    }
+    return res.status(409).json({
+      message: `A meal with this '${id}' is already in the meal options`,
+      status: 'Fail',
+    });
+  }
+}
+
+export default mealController;

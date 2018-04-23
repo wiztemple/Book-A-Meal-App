1b0b00f298fc9bfadeeba6a9b19cc6c0bbe62040
wiztemple <wiztemple7@gmail.com>
2018-04-23 14:46:07 +0100

    set up project by installing needed packages

---
+++ dummyServer/dummyData/index.js
@@ -0,0 +1,18 @@
+const menu = [
+  {
+    id: 1,
+    title: '',
+    day: '',
+    meals: [
+      {
+        id: 1,
+        title: 'Oha Soup',
+        description: 'Delicious Soup',
+        price: 700,
+        image: 'soup_image'
+      },
+    ]
+  }
+];
+
+export default menu;

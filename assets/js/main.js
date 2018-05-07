5913df871e60757c1281393347c28f97015dadf3
Okobia Joshua <okobia@cashenvoy.com>
2018-05-07 22:34:33 +0100

    minor fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,21 @@ function clickOutSide(e){
     modal.style.display = 'none';
   }
 };
+
+let modalBtn2 = document.getElementById('modalBtn2');
+let closeBtn2 = document.querySelector('.closeBtn2');
+
+modalBtn2.addEventListener('click', openModal2);
+closeBtn2.addEventListener('click', closeModal2);
+let modal2 = document.getElementById('simpleModal2');
+function openModal2(){
+  modal2.style.display = 'block';
+}
+function closeModal2(){
+  modal2.style.display = 'none';
+}
+function clickOutSide2(e){
+  if(e.target == modal2) {
+    modal2.style.display = 'none';
+  }
+};

5ba54199b69b881febc8fc4665049289d9180e7f
Okobia Joshua <okobia@cashenvoy.com>
2018-05-08 02:36:05 +0100

    add ID validation and password validation

---
+++ server/src/middlewares/validateId.js
@@ -0,0 +1,20 @@
+
+/**
+ * @description Validate recipe id
+ *
+ * @param {Objecty} request
+ * @param {Objecty} response
+ * @param {Function} next
+ *
+ * @returns {Object} Object
+ */
+const validateId = (request, response, next) => {
+  // If id is not a number
+  if (!Number(request.params.id)) {
+    return response.status(400).send({ status: 'Failure', message: 'Please input a valid ID' });
+  }
+  next();
+};
+
+export default validateId;
+

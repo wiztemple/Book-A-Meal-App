a6625ba5cf021b63b0f8cf8d8ec3ac36199a5756
Okobia Joshua <okobia@cashenvoy.com>
2018-05-11 04:26:02 +0100

    bug fixes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -17,14 +17,14 @@ const routes = (app) => {
   app.post('/api/v1/auth/login', checkLogin, UserController.login);
   app.get('/api/v1/auth/users', UserController.getAllUsers);
   app.post('/api/v1/meals', validate.mealFields, verifyToken, checkUser, MealController.addMeals);
-  app.delete('/api/v1/meals/:mealId', verifyToken, checkUser, MealController.deleteMeal);
-  app.put('/api/v1/meals/:mealId', verifyToken, checkUser, MealController.updateMeal);
+  app.delete('/api/v1/meals/:mealId', validateId, verifyToken, checkUser, MealController.deleteMeal);
+  app.put('/api/v1/meals/:mealId', validateId, verifyToken, checkUser, MealController.updateMeal);
   app.get('/api/v1/meals', verifyToken, checkUser, MealController.getAllMeals);
   app.post('/api/v1/menu', verifyToken, checkUser, MenuController.addMenu);
   app.get('/api/v1/menu', verifyToken, MenuController.getMenu);
-  app.post('/api/v1/order', verifyToken, OrderController.makeOrder);
-  app.put('/api/v1/order/:orderId', verifyToken, validateId, OrderController.updateOrder);
-  app.get('/api/v1/order', verifyToken, checkUser, OrderController.getOrders);
+  app.post('/api/v1/orders', verifyToken, OrderController.makeOrder);
+  app.put('/api/v1/orders/:orderId', verifyToken, validateId, OrderController.updateOrder);
+  app.get('/api/v1/orders', verifyToken, checkUser, OrderController.getOrders);
 };
 
 export default routes;
